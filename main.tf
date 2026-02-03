d0870cfdb63866dc8df3165ab073ba5dd5fd2e81
tyler <tyler@embold.com>
2026-02-02 18:34:21 -0800

    :bento: Update icons

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "coder_script" "reverse_proxy" {
   agent_id           = var.agent_id
   script             = templatefile("${path.module}/run.sh", { PROXY_LINE = join(" ", var.proxy_mappings) })
   display_name       = "Reverse Proxy"
-  icon               = "https://www.kali.org/tools/socat/images/socat-logo.svg"
+  icon               = "https://api.embold.net/icons?name=socat.svg"
   run_on_start       = true
   start_blocks_login = true
 }
